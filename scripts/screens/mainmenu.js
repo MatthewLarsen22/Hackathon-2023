eb24f1797ddb9fb31243be758f7f2b70b8eb728a
Matthew Larsen <matt4.0larsen@gmail.com>
2023-03-24 19:33:05 -0600

    hooking up main menu buttons

---
+++ scripts/screens/mainmenu.js
@@ -0,0 +1,29 @@
+MyGame.screens['mainMenu'] = (function(manager){
+    'use strict';
+
+    function initialize() {
+        document.getElementById('playGame').addEventListener(
+            'click',
+            function() {manager.showScreen('gamePlay'); }
+        );
+
+        document.getElementById('highScores').addEventListener(
+            'click',
+            function() { manager.showScreen('highScores'); }
+        );
+
+        document.getElementById('credits').addEventListener(
+            'click',
+            function() { manager.showScreen('credits'); }
+        );
+    }
+
+    function run() {
+
+    }
+
+    return {
+        initialize: initialize,
+        run: run
+    };
+}(MyGame.manager));
